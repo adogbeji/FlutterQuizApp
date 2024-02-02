36d6db33964428e7b765b4abfd3abc12bb914821
adogbeji <baka_1993@yahoo.co.uk>
2024-02-02 20:30:57 +0000

    Centre image in screen & add text

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,17 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.purpleAccent,
-        body: Column(
-          children: <Widget>[
-            Image.asset('assets/images/quiz-logo.png', width: 220,),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image.asset('assets/images/quiz-logo.png', width: 220,),
+              const Text('Learn Flutter the fun way!', style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),),
+            ],
+          ),
         ),
       ),
     )
