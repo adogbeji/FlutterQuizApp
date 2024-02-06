52b595633771cd6ab0173bd4d5bfe5a7476b8b41
adogbeji <baka_1993@yahoo.co.uk>
2024-02-06 20:44:21 +0000

    Change ElevatedButton to OutlinedButton in start screen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -6,40 +6,35 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 250,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                    Colors.purpleAccent,
-                  ),
-                  foregroundColor: MaterialStateProperty.all(
-                    Colors.white,
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text('Start Quiz'),
-              )
-            ],
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 250,
           ),
-        );
+          const SizedBox(
+            height: 30,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
   }
 }
