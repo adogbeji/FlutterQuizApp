a962a2dc6befe518ed84778ef99a2c88c19dd0c1
adogbeji <baka_1993@yahoo.co.uk>
2024-02-23 11:54:22 +0000

    Centre question and answers in questions screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,9 +12,28 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Questions Screen'),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
       ),
     );
   }
