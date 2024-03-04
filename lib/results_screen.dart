5556751e3641bca1b619df3f2697c3ea112bd2f0
adogbeji <baka_1993@yahoo.co.uk>
2024-03-04 11:31:01 +0000

    Start adding summary object in results screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_app/data/questions.dart';
+
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
 
-  List<Map<String, Object>> getSummaryData() {}
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': 1,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[1],
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +34,13 @@ class ResultsScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('You answered X out of Y quetsions correctly!'),
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             const Text('List of questions and answers...'),
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             TextButton(
               onPressed: () {},
               child: const Text('Restart Quiz!'),
