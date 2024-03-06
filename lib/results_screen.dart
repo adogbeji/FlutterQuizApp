20ba1aeafbdc74024eb3111da7dc9bcb5fa700a9
adogbeji <baka_1993@yahoo.co.uk>
2024-03-06 11:04:08 +0000

    Use QuestionsSummary widget in results screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
@@ -37,7 +38,8 @@ class ResultsScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text('List of questions and answers...'),
+            // const Text('List of questions and answers...'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(
               height: 30,
             ),
